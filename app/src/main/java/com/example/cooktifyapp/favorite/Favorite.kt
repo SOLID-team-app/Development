66a4263028547f5dcd77dc9f0e1630a415bbc4a8
Annisa Nadila <nadila.nasya@gmail.com>
2024-03-19 23:59:18 +0800

    buat package favorite

--- app/src/main/java/com/example/cooktifyapp/favorite/Favorite.kt
+++ app/src/main/java/com/example/cooktifyapp/favorite/Favorite.kt
@@ -1,7 +1,8 @@
-package com.example.cooktifyapp
+package com.example.cooktifyapp.favorite
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.cooktifyapp.R
 
 class Favorite : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
