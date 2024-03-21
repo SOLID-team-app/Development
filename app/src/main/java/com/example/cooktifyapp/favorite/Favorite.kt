e3892aa3729a3b549fc8901de02feb367237e8df
Annisa Nadila <nadila.nasya@gmail.com>
2024-03-21 14:10:31 +0800

    buat package favorite

--- app/src/main/java/com/example/cooktifyapp/favorite/Favorite.kt
+++ app/src/main/java/com/example/cooktifyapp/favorite/Favorite.kt
@@ -8,5 +8,6 @@ class Favorite : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_favorite)
+
     }
 }
