f5d6e9120036a0328926c775bdab71ebbb46be1f
EkaBayu <suputrabayu24@gmail.com>
2024-04-07 16:10:27 +0800

    terbaru 4/7/24 4:10 PM

--- app/src/main/java/com/example/cooktifyapp/view/navigation/home/Home.kt
+++ app/src/main/java/com/example/cooktifyapp/view/navigation/home/Home.kt
@@ -20,15 +20,16 @@ class Home : AppCompatActivity() {
 
         val navView : BottomNavigationView = binding.bottomBar
 
-        val navControler = findNavController(R.id.fragmentDashboard)
 
+        val navController = findNavController(R.id.fragmentDashboard)
+        // Passing each menu ID as a set of Ids because each
+        // menu should be considered as top level destinations.
         val appBarConfiguration = AppBarConfiguration.Builder(
-            R.id.navHome, R.id.navCamera, R.id.navFavorite, R.id.navProfile
+
+            R.id.nav_home, R.id.nav_camera, R.id.nav_favorite, R.id.nav_profil
+
         ).build()
-
-        setupActionBarWithNavController(navControler, appBarConfiguration)
-        navView.setupWithNavController(navControler)
-
-
+        setupActionBarWithNavController(navController, appBarConfiguration)
+        navView.setupWithNavController(navController)
     }
 }
